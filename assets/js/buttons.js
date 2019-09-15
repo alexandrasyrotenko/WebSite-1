bc3655cd6feb10de7ba810672c82ec520165463d
OlegMar1 <54880893+OlegMar1@users.noreply.github.com>
2019-09-15 17:39:16 +0300

    update sidebar, add header, add semantic ui

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,3 +1,8 @@
 function openMenu(){
   document.getElementById("sidebar").classList.toggle('active');
+  document.getElementById("head").classList.toggle('head-with-sidebar');
+}
+
+function openPoints(){
+  document.getElementByClassName("item").classList.toggle('activated');
 }
