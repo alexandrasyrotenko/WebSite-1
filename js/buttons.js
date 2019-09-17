0479103c44b73bd590a19174a878830e292b9b6c
OlegMar1 <54880893+OlegMar1@users.noreply.github.com>
2019-09-17 20:17:05 +0300

    DELETE SEMANTIC-UI GRID add right-sidebar fixed errors with adaptive header

--- js/buttons.js
+++ js/buttons.js
@@ -1,6 +1,8 @@
 function openMenu(){
   document.getElementById("sidebar").classList.toggle('active');
   document.getElementById("head").classList.toggle('head-with-sidebar');
+  document.getElementById("main").classList.toggle('main-with-sidebar');
+
 }
 
 function openPoints(){
